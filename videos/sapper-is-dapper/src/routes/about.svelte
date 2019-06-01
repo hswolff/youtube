0c955a2bb4c4420f6eb0e2b21c8e995ebc2bb70b
Harry Wolff <hswolff@gmail.com>
2019-05-31 21:51:42 -0400

    tweaks of sapper

--- videos/sapper-is-dapper/src/routes/about.svelte
+++ videos/sapper-is-dapper/src/routes/about.svelte
@@ -1,7 +1,9 @@
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+
+<h2>How's it going?</h2>
