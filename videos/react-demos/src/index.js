6e398bc83f8687d4c2b29325d7a396c0bf62ad9d
Harry Wolff <hswolff@gmail.com>
2019-08-16 20:28:55 -0400

    simplify react demos folder

--- videos/react-demos/src/index.js
+++ videos/react-demos/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React Demos</h1>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
