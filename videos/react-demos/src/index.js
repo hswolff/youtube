1906d4e834aa97b204a0ef580c84e156c316bc65
Harry Wolff <hswolff@gmail.com>
2020-04-25 08:03:01 -0400

    lazy load videos

--- videos/react-demos/src/index.js
+++ videos/react-demos/src/index.js
@@ -1,51 +1,50 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './index.css';
-
-import FramerMotion from './demo/framer-motion';
-import AsyncHooks from './demo/async-hooks';
 
 const routes = [
   {
     path: '/framer-motion',
     name: 'Framer Motion',
-    component: FramerMotion,
+    component: React.lazy(() => import('./demo/framer-motion')),
   },
   {
     path: '/async-hooks',
     name: 'Async React Hooks',
-    component: AsyncHooks,
+    component: React.lazy(() => import('./demo/async-hooks')),
   },
 ];
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <div>
-              <h1>React Demos</h1>
-              {routes.map((route, i) => (
-                <React.Fragment key={i}>
-                  <div>
-                    <Link to={route.path}>{route.name}</Link>
-                  </div>
-                  <br />
-                </React.Fragment>
-              ))}
-            </div>
-          )}
-        />
+    <Suspense fallback={null}>
+      <Router>
+        <div className='app'>
+          <Route
+            exact
+            path='/'
+            render={() => (
+              <div>
+                <h1>React Demos</h1>
+                {routes.map((route, i) => (
+                  <React.Fragment key={i}>
+                    <div>
+                      <Link to={route.path}>{route.name}</Link>
+                    </div>
+                    <br />
+                  </React.Fragment>
+                ))}
+              </div>
+            )}
+          />
 
-        {routes.map((route, i) => (
-          <Route key={i} {...route} />
-        ))}
-      </div>
-    </Router>
+          {routes.map((route, i) => (
+            <Route key={i} {...route} />
+          ))}
+        </div>
+      </Router>
+    </Suspense>
   );
 }
 
