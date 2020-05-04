626ae03f4db6a31e5aebb49aa2a5bc1179997728
Harry Wolff <hswolff@gmail.com>
2020-05-04 09:57:59 -0400

    implement it!

--- videos/react-demos/src/demo/use-event-listener.js
+++ videos/react-demos/src/demo/use-event-listener.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
 
-function useEventListener(eventType, handler) {
+function useEventListener(
+  eventType,
+  handler,
+  { enabled = true, target = document } = {}
+) {
   const handlerRef = useRef(handler);
 
   useEffect(() => {
@@ -8,27 +12,48 @@ function useEventListener(eventType, handler) {
   });
 
   useEffect(() => {
-    console.log('effect Ran');
+    if (!enabled) {
+      return;
+    }
     function internalHandler(e) {
       return handlerRef.current(e);
     }
 
-    document.addEventListener(eventType, internalHandler);
+    target.addEventListener(eventType, internalHandler);
 
-    return () => document.removeEventListener(eventType, internalHandler);
-  }, [eventType]);
+    return () => {
+      target.removeEventListener(eventType, internalHandler);
+    };
+  }, [eventType, enabled, target]);
 }
 
 export default function UseEventListenerPage() {
+  const [enabled, setEnabled] = useState(true);
   const [count, setCount] = useState(0);
 
-  useEventListener('click', () => {
-    console.log('I AM GLOBAL', count);
-  });
+  useEventListener(
+    'click',
+    () => {
+      //   console.log('I AM GLOBAL', count);
+    },
+    { enabled }
+  );
+
+  useEventListener(
+    'scroll',
+    () => {
+      console.log('THEY SEE ME SCROLLING');
+    },
+    { target: window }
+  );
 
   return (
-    <div>
+    <div style={{ height: '110vh' }}>
       <h1>useEventListener</h1>
+      <button onClick={() => setEnabled((e) => !e)}>
+        Enabled: {String(enabled)}
+      </button>
+      <br />
       <button onClick={() => setCount((c) => c + 1)}>Hello: {count}</button>
     </div>
   );
